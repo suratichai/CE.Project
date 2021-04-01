1e3ed7cc106b8f52a8a9f54ed795635b288a5014
suratichai <49265743+suratichai@users.noreply.github.com>
2021-04-01 14:16:26 +0700

    change source

---
+++ ComandList/raspberry pi mirror..sh
@@ -0,0 +1,5 @@
+sudo nano /etc/apt/sources.list
+
+deb http://raspbian.raspberrypi.org/raspbian/ stretch main contrib non-free rpi
+# Uncomment line below then 'apt-get update' to enable 'apt-get source'
+#deb-src http://raspbian.raspberrypi.org/raspbian/ stretch main contrib non-free rpi
