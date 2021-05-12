e8352df39aff768c1fc4ec24928b83fe391170d3
Penpitcha748 <70504446+Penpitcha748@users.noreply.github.com>
2021-05-13 00:21:45 +0700

    Update and rename Update necessary packages.txt to Update necessary packages.sh

--- ComandList/Update necessary packages.sh
+++ ComandList/Update necessary packages.sh
@@ -1,4 +1,4 @@
-sudo apt-get update
+sudo apt-get upgrade && sudo apt-get update
 sudo apt-get install -y rockchip-fstab rockchip-overlay
-sudo apt-get install -y linux-4.4-latest $ kernel version(>=4.4.154.100)
+sudo apt-get install -y linux-4.4-latest
 sudo apt-get install -y broadcom-wifibt-firmware(>=0.6)
